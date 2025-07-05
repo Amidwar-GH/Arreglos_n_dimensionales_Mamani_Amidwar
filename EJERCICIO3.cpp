c3bbd8986c4df256c04b2015f61ed3b9e07e2aeb
Amidwar <amidwar05@gmail.com>
2025-07-05 17:27:07 -0500

    EJERCICIO3: Suma de diagonales (VERSION FINAL)

--- EJERCICIO3.cpp
+++ EJERCICIO3.cpp
@@ -4,21 +4,48 @@ matriz de 4x4 y muestre el resultado. */
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+using namespace std;
 
 int main(){
-	
-	int Matriz[3][3];
+	int SumaDP=0;
+	int SumaDS=0;
+	int Matriz[4][4];
 	
 	srand(time(0));
 	
-	for(int i=0; i<3; i++){
-		for(int j=0; j<3; j++){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
 			Matriz[i][j] = rand()%9+1;
 		}
 	}
 	
+	cout<<"Matriz."<<endl;
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			cout<<Matriz[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 	
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			if(i==j){
+				SumaDP=SumaDP+Matriz[i][j];
+			}
+		}
+	}
 	
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			if(i+j == 3){
+				SumaDS=SumaDS+Matriz[i][j];
+			}
+		}
+		
+	}
+	cout<<"----------------------------------------"<<endl;
+	cout<<"La suma de la diagonal PRINCIPAL es: "<<SumaDP;
+	cout<<"\nLa suma de la diagonal SECUNDARIA es: "<<SumaDS;
 	
 	return 0;
 }
