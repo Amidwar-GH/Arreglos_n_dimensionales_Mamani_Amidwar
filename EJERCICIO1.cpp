5e989a7111bb86e353592ea350944f92f00059b2
Amidwar <amidwar05@gmail.com>
2025-07-05 11:43:17 -0500

    EJERCICIO1-AVANCE: Implementando el ejercicio1, inicializando el generador de numeros

---
+++ EJERCICIO1.cpp
@@ -0,0 +1,24 @@
+/*Producto de Dos Matrices: Implemente un programa que multiplique dos matrices de 3x3 y 
+muestre el resultado, y muestre las dos matrices y su resultado en horizontal 
+Ejemplo de Salida 
+Matriz A:             Matriz B:             Resultado: 
+1 2 3                    9 8 7                 30    24    18 
+4 5 6                    6 5 4                 84    69    54 
+7 8 9                    3 2 1                 138  114  90*/
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main (){
+	int matrizA[3][3];
+	int matrizB[3][3];
+	
+	srand(time(0));
+	
+	
+	
+	
+	return 0;
+}
