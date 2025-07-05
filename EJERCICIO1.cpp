a66316f170674749631d394171f930c69f739c32
Amidwar <amidwar05@gmail.com>
2025-07-05 12:24:03 -0500

    EJERCICIO1-AVANCE: Agregando el algoritmo de multiplicacion

--- EJERCICIO1.cpp
+++ EJERCICIO1.cpp
@@ -14,11 +14,49 @@ using namespace std;
 int main (){
 	int matrizA[3][3];
 	int matrizB[3][3];
+	int resultado[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
 	
 	srand(time(0));
 	
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; j++){
+			matrizA[i][j] = rand()%9+1;
+			matrizB[i][j] = rand()%9+1;
+		}
+	}
 	
 	
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; j++){
+			for(int k=0; k<3; k++){
+				resultado[i][j] = resultado[i][j] + matrizA[i][k]*matrizB[k][j];
+			}
+		}
+	}
 	
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; j++){
+			cout<<matrizA[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	cout<<endl;
+	
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; j++){
+			cout<<matrizB[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	cout<<endl;
+	
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3; j++){
+			cout<<resultado[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 	return 0;
 }
