5e4ea703db5f6d238638a894480b314df283d965
Amidwar <amidwar05@gmail.com>
2025-07-06 14:09:21 -0500

    EJERCICIO7- Rotando una matriz (VERSION FINAL)

--- EJERCICIO7.cpp
+++ EJERCICIO7.cpp
@@ -34,20 +34,18 @@ int main(){
 		}
 	}
 	
+	cout<<"Matriz          Matriz rotada 90 grados en sentido horario"<<endl;
 	for(int i=0; i<4; i++){
 		for(int j=0; j<4; j++){
 			cout<<Matriz[i][j]<<" ";
 		}
-		cout<<endl;
-	}
-	
-	cout<<endl;
-	
-	for(int i=0; i<4; i++){
+		cout<<"\t";
+		
 		for(int j=0; j<4; j++){
 			cout<<MatrizRotada[i][j]<<" ";
 		}
 		cout<<endl;
 	}
+	
 	return 0;
 }
