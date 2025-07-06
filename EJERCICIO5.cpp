cea59d95692d876634e6265cc7d5173e91d9399d
Amidwar <amidwar05@gmail.com>
2025-07-06 12:42:44 -0500

    EJERCICIO5: Busqueda del mayor y menor valor (VERSION FINAL)

--- EJERCICIO5.cpp
+++ EJERCICIO5.cpp
@@ -10,6 +10,7 @@ int main(){
 	int Matriz[4][4];
 	int MAXIMO ;
 	int MINIMO;
+	int F,C,F1,C1;
 	
 	srand(time(0));
 	
@@ -19,11 +20,54 @@ int main(){
 		}
 	}
 	
+	cout<<"Matriz"<<endl;
 	for(int i=0; i<4; i++){
 		for(int j=0; j<4; j++){
 			cout<<Matriz[i][j]<<" ";
 		}
 		cout<<endl;
+	}
+	
+	MAXIMO = Matriz[0][0];
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			if(Matriz[i][j]>MAXIMO){
+				MAXIMO = Matriz[i][j];
+				F=i;
+				C=j;
+			}
+		}
+		cout<<endl;
+	}
+	cout<<"-----------------------------------------------"<<endl;
+	
+	cout<<"La posicion o posiciones del maximo valor es:  "<<endl;
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			if(Matriz[i][j] == MAXIMO){
+				cout<<"["<<i<<","<<j<<"]"<<endl;
+			}
+		}
+	}
+	
+	MINIMO = Matriz[0][0];
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			if(Matriz[i][j]<MINIMO){
+				MINIMO = Matriz[i][j];
+				F1 = i;
+				C1 = j;
+			}
+		}
+	}
+	
+	cout<<"La posicion o posiciones del minimo valor es:  "<<endl;
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			if(Matriz[i][j] == MINIMO){
+				cout<<"["<<i<<","<<j<<"]"<<endl;
+			}
+		}
 	}
 	
 	return 0;
